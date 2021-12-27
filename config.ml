12bcd91a49f2a145fc5d7f0be646e62fa80001e8
yomimono <maintenance@identity-function.com>
2021-12-26 19:24:36 -0600

    start trying to get le in there

--- config.ml
+++ config.ml
@@ -2,5 +2,18 @@
 
 open Mirage
 
+let packages = [ Functoria.package ~sublibs:["kv"] "chamelon";
+                 Functoria.package ~sublibs:["ocaml"] "digestif";
+                 Functoria.package ~sublibs:["ocaml"] "checkseum";
+               ]
+
+let stack = generic_stackv4v6 default_network
+let http_srv = cohttp_server @@ conduit_direct stack
+
+let block_imp = generic_block "shortener"
+
 let main =
-  foreign ~packages:[] ~keys:[] "Shortener.Main" (block @-> http @-> job)
+  foreign ~packages ~keys:[] "Shortener.Main" (block @-> pclock @-> http @-> job)
+
+let () =
+  register "shortener" [ main $ block_imp $ default_posix_clock $ http_srv ]
--- shortener.ml
+++ shortener.ml
@@ -1,4 +1,14 @@
-module Main(Block : Mirage_block.S)(Http : Mirage_http.S) = struct
+module Main
+    (Block : Mirage_block.S)
+    (Clock : Mirage_clock.PCLOCK)
+    (Http : Cohttp_mirage.Server.S)
+= struct
+  module Logs_reporter = Mirage_logs.Make(Clock)
+  module Kv = Kv.Make(Block)(Clock)
 
-  let main _ _ = ()
+  let start block pclock _http =
+    let open Lwt.Infix in
+    Logs_reporter.(create pclock |> run) @@ fun () ->
+    Kv.connect ~program_block_size:16 ~block_size:4096 block >>= fun kv ->
+    Lwt.return_unit
 end
